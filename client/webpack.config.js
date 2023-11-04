6c9c5112f0b5c3d8a5b694e6142076891f39e1d0
Nick Edwards <theadventuresofnickedwards@gmail.com>
2023-11-03 17:25:02 -0700

    Restoring webpack InjectManifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,8 +27,7 @@ module.exports = () => {
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
-        // swDest: 'src-sw.js', FIX 11/3/2023
-        swDest: './src-sw.js',
+        swDest: 'src-sw.js',
       }),
       //////////
       // REF: 19.3.26 - Manifest
